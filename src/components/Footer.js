da32e3178d7f853cde8f12a2184717b42e14e66e
Chizo Nwazuo <chizonwazuo@yahoo.com>
2021-08-31 15:15:26 +0000

    :chore: add extra footer note of SSR experiment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,10 @@ export default function Footer() {
         <div className="h-0.5 w-full bg-white dark:bg-gray-700"></div>
         <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between md:items-center mt-8">
           <div>
-            <p>&copy; Chizo. All Rights Reserved.</p>
+            <p>
+              &copy; Chizo. This website is my React SSR experiment <br /> and
+              is not optimized for production(will do that later).
+            </p>
           </div>
           <div>
             <p>
